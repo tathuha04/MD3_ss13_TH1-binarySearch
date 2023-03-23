ecf044aa6890c167a0d6eb90dcedaccc891dbb62
TaThuHa <thuhak2tltnpt@gmail.com>
2023-03-23 09:23:19 +0700

    update

---
+++ src/rikkei/academy/BinarySearch.java
@@ -0,0 +1,36 @@
+package rikkei.academy;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));
+        System.out.println(binarySearch(list, 3));
+        System.out.println(binarySearch(list, 4));
+        System.out.println(binarySearch(list, 7));
+        System.out.println(binarySearch(list, 9));
+        System.out.println(binarySearch(list, 10));
+        System.out.println(binarySearch(list, 11));
+        System.out.println(binarySearch(list, 79));
+        System.out.println(binarySearch(list, 1));
+        System.out.println(binarySearch(list, 5));
+        System.out.println(binarySearch(list, 80));
+
+    }
+
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int hight = list.length - 1;
+        while (hight >= low) {
+            int mid = (low + hight) / 2;
+            if (key < list[mid]) {
+                hight = mid - 1;
+            } else if (key == list[mid]) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
